99d4917d8f13440368110ac77c0e6f900d94d924
Ilyass El Fatimy <ilyasselfatimy9@gmail.com>
2021-05-11 13:15:47 +0000

    Update __init__.py

--- luhn_check/__init__.py
+++ luhn_check/__init__.py
@@ -1,1 +1,5 @@
-from .luhn import Luhn
+def check(n):
+    r = [int(_) for _ in str(n)[::-1]]
+    checksum = (sum(r[0::2]) + sum(sum(divmod(d*2, 10))
+                                   for d in r[1::2]))
+    return checksum % 10 == 0 and checksum
